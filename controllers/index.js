4c20cff59cf9ac639c40fcbc461cbb434f47f7d6
Me <justin_hunter@hotmail.com>
2024-04-14 16:04:51 -0600

    initial structure and direction

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const postRoutes = require("./api");
 const getRoutes = require("./get");
 
-router.use("/", getRoutes)
+router.use("/", getRoutes);
 router.use("/api", postRoutes);
 
 
